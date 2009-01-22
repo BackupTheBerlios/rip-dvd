8fb5a28a7794687175a3e879f7ec797acc8f0852
Thomas Spurden <thomasspurden@gmail.com>
2009-01-22 08:46:52 +0000

    Can copy audio directly instead of encoding

--- scripts/rip.mk
+++ scripts/rip.mk
@@ -3,8 +3,20 @@
 " = "
 SOURCE = -dvd-device $(CONFIG_SOURCE_DVD) dvd://$(CURTITLE) -cache $(CONFIG_SOURCE_CACHE) $(if $(subst $",,$($(CFG_PREFIX)START)),-ss $($(CFG_PREFIX)START)) $(if $(subst $",,$($(CFG_PREFIX)LENGTH)),-endpos $($(CFG_PREFIX)LENGTH))
 
-AUDIO_TARGETS = $(foreach a,$($(CFG_PREFIX)AUDIOS),$(if $($(CFG_PREFIX)AUDIO_$(a)_COPY),audio-$(a).ac3,audio-$(a).ogg))
+define audio_target
+ifeq ($$($$(CFG_PREFIX)AUDIO_$(1)_COPY),y)
+AUDIO_TARGET_$(1) = audio-$(1).avi
+else
+AUDIO_TARGET_$(1) = audio-$(1).ogg
+endif
+AUDIO_TARGETS += audio-$(1).lang $$(AUDIO_TARGET_$(1))
+endef
+
+AUDIO_TARGETS :=
+$(foreach a,$($(CFG_PREFIX)AUDIOS),$(eval $(call audio_target,$(a))))
 SUBTITLE_TARGETS = $(foreach s,$($(CFG_PREFIX)SUBTITLES),sub-$(s).idx)
+
+FIRST_AUDIO := $(firstword $($(CFG_PREFIX)AUDIOS))
 
 ifeq ($($(CFG_PREFIX)DEINTERLACE),y)
 DEINTERLACE := -vf-add pp=lb
@@ -36,25 +48,44 @@ endif
 
 video.avi : autocrop.txt
 	@echo "VIDEO   $($(CFG_PREFIX)NAME)"
-	@mencoderwrap "$($(CFG_PREFIX)NAME) (video)" $(SOURCE) -ovc x264 -oac copy \
+	@mencoderwrap "$($(CFG_PREFIX)NAME) (video)" $(SOURCE) -ovc x264 -oac copy $(if $(FIRST_AUDIO),-aid $(FIRST_AUDIO)) \
 		-x264encopts crf=$($(CFG_PREFIX)VIDEO_CRF):frameref=6:bframes=4:b_adapt:b_pyramid:direct_pred=auto:partitions=all:8x8dct:me=umh:subq=7:weight_b:brdo:bime:trellis=2:threads=$($(CFG_PREFIX)VIDEO_THREADS) \
 		-vf-add crop=$$(cat $^) $(DEINTERLACE) \
 		-vf-add harddup \
 		-o "$@"
-audio-%.ogg :
+
+#audio-$(FIRST_AUDIO).avi :
+#	@echo "  AID $* link $($(CFG_PREFIX)NAME)"
+#	@ln -s video.avi "$@"
+
+# Always rip from an avi that has been ripped with both audio & video.  This prevents AV desync due to -ss skipping to
+# different places when the video is disabled
+# prevent make deleting the avi
+.PRECIOUS : audio-%.avi
+audio-%.avi :
+	@echo "  AID $* copy $($(CFG_PREFIX)NAME)"
+# The crop filter greatly reduces the amount of space the avi takes up (have to use raw, copy doesnt get vfs applied)
+	@mencoderwrap "$($(CFG_PREFIX)NAME) (AID $* copy)" $(SOURCE) -oac copy -ovc raw -vf crop=1:1 -aid $* -o "$@.tmp"
+	@mv "$@.tmp" "$@"
+
+audio-%.lang :
+	@awk -F "=" '/ID_AID_$*_LANG/ { printf $$2; }' < "identify.txt" > "$@.tmp"
+	@test -s "$@.tmp"
+	@mv "$@.tmp" "$@"
+
+audio-%.ogg : audio-%.avi
 	@echo "  AID $* (vorbis) $($(CFG_PREFIX)NAME)"
 	@rm -f "$@.fifo"
 	@mkfifo "$@.fifo"
-	@awk -F "=" '/ID_AID_$*_LANG/ { printf $$2; }' < "identify.txt" > "audio-$*.lang"
 	@oggencwrap "$($(CFG_PREFIX)NAME) (AID $* vorbis)" -q $($(CFG_PREFIX)AUDIO_$*_QUALITY) -o "$@.tmp" "$@.fifo" &
-	@mplayer $(SOURCE) -hardframedrop -quiet -aid $* -vo null -ao pcm:fast:file="$@.fifo" > "$@.log" 2>&1
+	@mplayer "audio-$*.avi" -hardframedrop -quiet -vo null -ao pcm:fast:file="$@.fifo" > "$@.log" 2>&1
 	@mv "$@.tmp" "$@"
 	@rm "$@.fifo"
 
-audio-%.ac3 :
-	@echo "  AID $* (ac3) $($(CFG_PREFIX)NAME)"
-	@mencoderwrap "$($(CFG_PREFIX)NAME) (AID $* ac3)" $(SOURCE) -oac copy -ovc copy -of rawaudio -aid $* -o "$@.tmp"
-	@mv "$@.tmp" "$@"
+#audio-%.ac3 : audio-%.avi audio-%.lang
+#	@echo "  AID $* (ac3) $($(CFG_PREFIX)NAME)"
+#	@mencoderwrap "$($(CFG_PREFIX)NAME) (AID $* ac3)" "audio-$*.avi" -oac copy -ovc copy -of rawaudio -o "$@.tmp"
+#	@mv "$@.tmp" "$@"
 
 sub-%.idx :
 	@echo "   SID $* $($(CFG_PREFIX)NAME)"
@@ -64,11 +95,10 @@ sub-%.idx :
 	@mv "sub-$*.tmp.sub" "sub-$*.sub"
 	
 
-output.mkv : identify.txt chapters.txt $(SUBTITLE_TARGETS) $(AUDIO_TARGETS) video.avi
-	@echo $^
+output.mkv : video.avi identify.txt chapters.txt $(SUBTITLE_TARGETS) $(AUDIO_TARGETS)
 	@echo "  MUX   $($(CFG_PREFIX)NAME)"
-	@-mkvmerge -o "$@.tmp" --chapters chapters.txt --title "$($(CFG_PREFIX)NAME)" -A video.avi \
-		$(foreach aid,$($(CFG_PREFIX)AUDIOS),--language 0:$$(cat "audio-$(aid).lang") "audio-$(aid).ogg") \
+	@-mkvmerge -o "$@.tmp" --chapters chapters.txt --title "$($(CFG_PREFIX)NAME)" --noaudio video.avi \
+		$(foreach aid,$($(CFG_PREFIX)AUDIOS),--language 0:$$(cat "audio-$(aid).lang") --novideo "$(AUDIO_TARGET_$(aid))") \
 		$(foreach sid,$($(CFG_PREFIX)SUBTITLES),"sub-$(sid).idx") > mux.log 2>&1
 	@mv "$@.tmp" "$@"
 
